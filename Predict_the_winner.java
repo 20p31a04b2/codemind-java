26d5717af57fd1c6d0bb60e28b4653fe6ceeace2
20p31a04b2 <112801512+20p31a04b2@users.noreply.github.com>
2023-01-22 19:39:55 +0530

    import java.util.Scanner; class winner {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x1=0,y1=0,d1;         n=sc.nextInt();         int x[]=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();             if(i%2==0)              x1+=x[i];             else              x1+=x[i];
            }
            d1=x1-y1;
            if(d1<0)
             d1=d1/-1;
            if(d1%4==0)
            System.out.println("X");
            else
            System.out.println("Y");
             
        }
    }

---
+++ Predict_the_winner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class winner
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x1=0,y1=0,d1;
+        n=sc.nextInt();
+        int x[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+            if(i%2==0)
+             x1+=x[i];
+            else
+             x1+=x[i];
+            
+        }
+        d1=x1-y1;
+        if(d1<0)
+         d1=d1/-1;
+        if(d1%4==0)
+        System.out.println("X");
+        else
+        System.out.println("Y");
+         
+    }
+}
