f4ada07e9a2299a639dee005b6797ecbb38c4090
20p31a04b2 <112801512+20p31a04b2@users.noreply.github.com>
2023-01-31 15:48:43 +0530

    import java.util.*; class demo {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         int n=s1.length();         int wc=0;         for(int i=0;i<n;i++)         {             char ch=s1.charAt(i);             if(ch == ' ' && s1.charAt(i+1)!=' ')             {                 wc++;             }         }         System.out.println(wc+1);     } }

---
+++ Count_no._of_words.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class demo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int n=s1.length();
+        int wc=0;
+        for(int i=0;i<n;i++)
+        {
+            char ch=s1.charAt(i);
+            if(ch == ' ' && s1.charAt(i+1)!=' ')
+            {
+                wc++;
+            }
+        }
+        System.out.println(wc+1);
+    }
+}
